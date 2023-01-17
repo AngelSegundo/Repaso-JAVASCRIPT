d1874e617792273549ff06dc5828de8219831370
Angel Rodriguez <angel.segundo.dev@gmail.com>
2023-01-17 23:22:36 +0100

    DONE

--- main.js
+++ main.js
@@ -16,17 +16,47 @@ function random(min, max) {
     }
 }
 
+addEventListener('keypress', (e) => { enter(e) })
+
+function enter(e) {
+    let tecla = (document) ? e.keyCode : e.which;
+    if (tecla == 13) {
+        random(1, 10)
+
+        let randomNumber = JSON.parse(localStorage.getItem('randomNumber'))
+        let numOption
+
+        option.value ? numOption = option.value : alert('Introduce tu respuesta')
+
+        if (option.value < 1 || option.value > 10) {
+            alert('Tu respuesta debe estar entre 1 y 10')
+            return
+        } else {
+            if (option.value > randomNumber) {
+                answer.textContent = `${numOption} es mayor`
+                return
+            } if (option.value < randomNumber) {
+                answer.textContent = `${numOption} es menor`
+            } else {
+                answer.textContent = `Enhorabuena!!!  Estaba pensando en el número ${numOption}. Juguemos de nuevo!`
+                localStorage.clear()
+            }
+        }
+
+    }
+}
+
 calculator.onclick = () => {
 
-    random(1, 50)
+    random(1, 10)
 
     let randomNumber = JSON.parse(localStorage.getItem('randomNumber'))
     let numOption
 
     option.value ? numOption = option.value : alert('Introduce tu respuesta')
 
-    if (option.value < 1 || option.value > 50) {
-        alert('Tu respuesta debe estar entre 1 y 50')
+    if (option.value < 1 || option.value > 10) {
+        alert('Tu respuesta debe estar entre 1 y 10')
         return
     } else {
         if (option.value > randomNumber) {
@@ -43,6 +73,65 @@ calculator.onclick = () => {
 
 //--- --- --- --- --- EJERCICIO 2 --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---//
 
+let exercise2 = document.querySelector(".exercise2")
+let start = document.querySelector('#start')
+
+start.onclick = () => {
+    let option1 = document.getElementById('option1')
+    let option2 = document.getElementById('option2')
+    let option3 = document.getElementById('option3')
+    let higher = document.getElementById('higher')
+
+    num1 = prompt("Primer número") * 1
+    num2 = prompt("Segundo número") * 1
+    num3 = prompt("Tercer número") * 1
+
+    option1.textContent = num1
+    option2.textContent = num2
+    option3.textContent = num3
+
+    numArary = []
+    higherNum = 0
+
+    numArary.push(num1, num2, num3)
+
+    numArary.forEach(num => {
+        if (higherNum < num) {
+            higherNum = num
+        }
+    });
+
+    higher.textContent = `El número mayor es ${higherNum}`
+
+}
 
 
 //--- --- --- --- --- EJERCICIO 3 --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---//
+document.addEventListener('DOMContentLoaded', () => {
+    fetchData()
+})
+
+const fetchData = async () => {
+    try {
+        const res = await fetch('apis.json')
+        const studentsList = await res.json()
+        showStudents(studentsList)
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const showStudents = studentsList => {
+    studentsList.sort((a, b) => b.age - a.age)
+    studentsList.forEach(student => {
+        let studentInfo = document.createElement('div')
+        studentInfo.setAttribute('class', 'card')
+        document.querySelector('.container').appendChild(studentInfo)
+        studentInfo.innerHTML = `
+        <h4>Hover Me</h4>
+                        <span>${student.name}</span>
+
+                        <span>${student.age}</span>
+            `
+    });
+}
